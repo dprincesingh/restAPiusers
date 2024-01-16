0631884caf131e596dfdcb7fe8dbd224f64f7291
dprincesingh <theprincesingh07@gmail.com>
2024-01-17 03:17:46 +0530

    env added

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import express from "express";
 import connectMongodb from "./src/Config/connection.js";
 import useRouter from "./src/Routes/loanuser.js";
+import 'dotenv/config'
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-const port = 3000;
+const port = 4000;
 
 
-connectMongodb("mongodb://localhost:27017/loanuser")
-  .then(() => console.log("Connected to MongoDb"))
-  .catch((err) => console.error("something went wrong .. " + err));
+try{
+
+  connectMongodb("mongodb://localhost:27017/loanuser")
+}catch(err){
+  console.log(err);
+}
+  
 
 app.use("/api/loanuser", useRouter);
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log("Port is running at " + port);
 });
